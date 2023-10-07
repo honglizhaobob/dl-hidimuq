912d9e0d9f616f0ead63c82382e7434e7dcd56a7
Hongli (Bob) Zhao <honglizhaobob@berkeley.edu>
2023-10-07 00:12:36 -0500

    cleaned up linear oscillator problem

--- code/linear_oscillator/matlab/exp_cov.m
+++
@@ -1,6 +0,0 @@
-function R = exp_cov(K, lam)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Exponential spatial correlation, proportional to stiffness matrix K.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-R = exp(-K./lam);
-end
